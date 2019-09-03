81470b0c963e7b409cad1e80aff737cab0460552
Kristina <kristina.tanya@gmail.com>
2019-09-03 15:00:32 -0700

    Made create_rooms method in Hotel class to instantiate 20 rooms.

--- test/hotel_test.rb
+++ test/hotel_test.rb
@@ -3,12 +3,18 @@ require_relative 'test_helper'
 describe "Hotel class" do 
   describe "Hotel instantiation" do
     before do
-      @hotel = HotelSystem::Hotel.new
+      @hotel = HotelSystem::Hotel.new(20)
     end
     
     it "is an instance of Hotel" do 
       expect(@hotel).must_be_kind_of HotelSystem::Hotel
     end
+    
+    it "will create the correct number of rooms" do
+      @hotel.create_rooms
+      p @hotel.rooms
+      expect(@hotel.rooms.length).must_equal 20
+    end 
     
     
   end
