d1faf4e8751be0ee35dad0e844af498e6e903e66
Kristina <kristina.tanya@gmail.com>
2019-09-03 21:32:42 -0700

    Added scaffolding for future methods to implement in Date_Utility.

--- lib/date_utility.rb
+++ lib/date_utility.rb
@@ -2,9 +2,15 @@ module HotelSystem
   class Date_Utility
     def initialize
       
-      
-      
-      
+    end
+    
+    def check_date_overlap
+      # take in a date and check if it overlaps with other reserved dates
+    end 
+    
+    def validate_dates
+      # check if date range is valid
+      # end_date can't be before start_date
     end
     
   end
