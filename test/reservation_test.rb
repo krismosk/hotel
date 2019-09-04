a3d2377daad9e7e6fbf679a5be359f2112b92a38
Kristina <kristina.tanya@gmail.com>
2019-09-04 11:15:28 -0700

    Added test to check Reservation can be instantiated with a reservation number.

--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -3,13 +3,16 @@ require_relative 'test_helper'
 describe "Reservation class" do 
   describe "Reservation instantiation" do
     before do
-      @reservation = HotelSystem::Reservation.new
+      @reservation = HotelSystem::Reservation.new(101)
     end
     
     it "is an instance of Reservation" do 
       expect(@reservation).must_be_kind_of HotelSystem::Reservation
     end
     
+    it "knows it's room number" do
+      expect(@reservation.reservation_id).wont_be_nil
+    end
     
   end
   
